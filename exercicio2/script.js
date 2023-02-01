98e32473cf682f354470f6f412d25c832c2cfe29
Cristian91Assuncao <cristian.assuncao@hotmail.com>
2023-02-01 16:11:38 -0300

    Resolução de exercícios

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let a = 10
+let b = 20
+let c = a
+
+console.log('O valor de a é:', a)
+console.log('O valor de b é:', b)
+
+a = b
+b = c
+
+console.log('TROCANDO OS VALORES DE a & b')
+console.log('O novo valor de a é:', a)
+console.log('O novo valor de b é:', b)
